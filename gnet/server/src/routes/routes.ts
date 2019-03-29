f5871f644f390cdfb01b8871db1b1dd9f4548bb2
Arthur Johas Matta <up201609953@fe.up.pt>
2019-03-29 17:58:28 +0000

    Moved gnet folder content to root folder

--- gnet/server/src/routes/routes.ts
+++
@@ -1,5 +0,0 @@
-export * from './feed';
-export * from './login';
-export * from './post';
-export * from './users';
-export * from './admin';
---
+++ server/src/routes/routes.ts
@@ -0,0 +1,5 @@
+export * from './feed';
+export * from './login';
+export * from './post';
+export * from './users';
+export * from './admin';
