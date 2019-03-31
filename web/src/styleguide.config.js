8261d22d92599fe086765d9ca354ea963b78c5a5
Arthur Johas Matta <up201609953@fe.up.pt>
2019-03-31 16:40:52 +0100

    Fixed and updated configuration files

--- web/src/styleguide.config.js
+++ web/src/styleguide.config.js
@@ -1,6 +1,7 @@
+// @ts-ignore
 module.exports = {
   components: "./components/**/*.{js,jsx,ts,tsx}",
-  propsParser: require("react-docgen-typescript/lib/index").withDefaultConfig({
+  propsParser: require("react-docgen-typescript").withDefaultConfig({
     propFilter: {
       skipPropsWithoutDoc: true
     }
