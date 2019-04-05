cbdc02d02b1ec6ea9689d9d342ebe788354303c5
Daniel Marques <up201503822@fe.up.pt>
2019-04-05 17:58:16 +0100

    Initial feed

---
+++ web/src/views/Feed.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+class Profile extends React.Component {
+  public render() {
+    return <div className="Feed">Hello feed</div>;
+  }
+}
+
+export default Profile;
