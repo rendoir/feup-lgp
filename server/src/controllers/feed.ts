533ff0376fea282190ef7b746954da0b2a20ac85
Joao611 <joaofvde@gmail.com>
2019-04-06 01:18:38 +0100

    Implement backend's feed request

--- server/src/controllers/feed.ts
+++ server/src/controllers/feed.ts
@@ -1,5 +1,25 @@
 'use strict';
 
-export function getFeed(req, res) {
-    res.send("Hello from node feed");
+import { query } from '../db/db';
+
+export async function getFeed(req, res) {
+    const offset = req.query.offset;
+    const userId = 1;
+    try {
+        const result = await query({
+            text: `SELECT id, author, title, content, date_created, date_updated
+                    FROM posts
+                    WHERE
+                        author = $1
+                        OR author IN (SELECT followed FROM follows WHERE follower = $1)
+                    ORDER BY date_created
+                    LIMIT 10
+                    OFFSET $2`,
+            values: [userId, offset],
+        });
+        res.send(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(new Error('Error retrieving feed'));
+    }
 }
