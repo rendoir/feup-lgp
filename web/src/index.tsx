0dda0c8c2bc19fc022848f6d04076b39be93798d
Joao611 <joaofvde@gmail.com>
2019-04-12 22:23:06 +0100

    Setup linter

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,6 @@
+import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import "@fortawesome/fontawesome-free/css/all.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
