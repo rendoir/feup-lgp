7ea86382cf0e706f621e994a3d3ad85501e72501
Daniel Marques <up201503822@fe.up.pt>
2019-04-14 16:31:54 +0100

    Added conferences to feed. CSS tweaks

--- web/src/views/Feed.tsx
+++ web/src/views/Feed.tsx
@@ -51,12 +51,27 @@ class Feed extends React.Component<Props, State> {
       />
     ));
 
+    let hardCodedConferences = [
+      "Conference 1",
+      "Conference 45465",
+      "Conference 45",
+      "Conference 46848",
+      "Big Conference name to test css properly, omg this name is not over yet"
+    ];
+    let conferences = hardCodedConferences.map(title => (
+      <a key={title} className="conference-link d-block my-2">
+        {title}
+      </a>
+    ));
+
     return (
       <div id="Feed" className="container my-5">
         <div className="row">
-          <div className="left col-lg-2">LEFT</div>
+          <div className="left col-lg-3 mr-5">
+            <h5>Conferences</h5>
+            {conferences}
+          </div>
           <div className="middle col-lg-8">{posts}</div>
-          <div className="right col-lg-2">RIGHT</div>
         </div>
       </div>
     );
