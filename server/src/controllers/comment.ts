0d30379f8de9da281c31dc275b410e7eeae9094c
Toliveira97 <toliveira_97@hotmail.com>
2019-04-18 18:47:53 +0200

    Started deleting comments

---
+++ server/src/controllers/comment.ts
@@ -0,0 +1,54 @@
+import * as fs from 'fs';
+import * as request from 'request-promise';
+// import * as Twitter from 'twitter';
+
+import {query} from '../db/db';
+
+export function createComment(req, res) {
+    if(!req.body.comment.trim() || !req.body.comment.trim()) {
+        console.log('\n\nERROR: Comment body cannot be empty');
+        res.status(400).send({ message: 'An error ocurred while creating a new comment' });
+        return;
+    }
+
+    query({
+        // Add image, video and document when we figure out how to store them (Update route documentation after adding them)
+        text: 'INSERT INTO comments (author, post, comment) VALUES ($1, $2, $3) RETURNING id',
+        values: [req.body.author, req.body.post, req.body.comment],
+    }).then((result) => {
+        res.send({id: result.rows});
+    }).catch((error) => {
+        console.log('\n\nERROR:', error);
+        res.status(400).send({ message: 'An error ocurred while creating a new comment' });
+    });
+}
+
+export function editPost(req, res) {
+    if(!req.body.comment.trim() || !req.body.comment.trim()) {
+        console.log('\n\nERROR: Comment body cannot be empty');
+        res.status(400).send({ message: 'An error ocurred while editing a comment' });
+        return;
+    }
+
+    query({
+        // Add image, video and document when we figure out how to store them (Update route documentation after adding them)
+        text: 'UPDATE comments SET post=$2, comment=$3 WHERE id=$1',
+        values: [req.body.id, req.body.post, req.body.comment],
+    }).then((result) => {
+        res.status(200).send();
+    }).catch((error) => {
+        console.log('\n\nERROR:', error);
+        res.status(400).send({ message: 'An error ocurred while editing a comment' });
+    });
+}
+
+export function deletePost(req, res) {
+    query({
+        text: 'DELETE FROM comments WHERE id=$1', values: [req.body.id],
+    }).then((result) => {
+        res.status(200).send();
+    }).catch((error) => {
+        console.log('\n\nERROR:', error);
+        res.status(400).send({ message: 'An error ocurred while deleting a comment' });
+    });
+}
