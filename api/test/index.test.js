22438692d4a2d55d794c34db56f0ac19b77d8cd8
Joao611 <joaofvde@gmail.com>
2019-04-25 00:08:23 +0100

    Install various API test dependencies and start test development in general

---
+++ api/test/index.test.js
@@ -0,0 +1,67 @@
+process.env.TEST = 'true';
+const expect = require('chai').expect;
+const app = require('../dist/index.js').app;
+const request = require('supertest');
+const db = require('../dist/db/db');
+const dotenv = require("dotenv");
+
+// now + 3 days
+let futureDate = new Date();
+futureDate.setDate(futureDate.getDate() + 3);
+const futureDateStr = '"' + futureDate.toISOString() + '"';
+
+function loadEnvironment() {
+    if (process.env.PRODUCTION === 'true') {
+        console.log('IN PROD');
+        dotenv.config({ path: 'docker/prod.env' });
+        dotenv.config({ path: 'docker/secrets.env' });
+    }
+    else {
+        console.log('NOT IN PROD');
+        dotenv.config({ path: '../../environment/dev.env' }); // dotenv is used to load env variables
+        dotenv.config({ path: '../../secrets/secrets.env' });
+    }
+}
+
+async function getTableNames() {
+    return db.query({
+        text: `SELECT table_name
+                FROM information_schema.tables
+                WHERE table_type = 'BASE TABLE'
+                    AND table_schema NOT IN ('pg_catalog', 'information_schema');`
+    });
+}
+
+function cleanTable(table) {
+    db.query({
+        text: `TRUNCATE ${table} CASCADE`, // cannot parameterize table names
+    });
+}
+
+async function cleanDb() {
+    try {
+        const tableNames = await getTableNames();
+        for (const row of tableNames.rows) {
+            cleanTable(row.table_name);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function insertAdminUser() {
+    // return db.query({
+    //     text: `INSERT INTO users (email, pass, permissions)
+    //         VALUES ('admin@gmail.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin')
+    //         RETURNING id`,
+    // }).then((res) => res.rows[0].id);
+}
+
+before((done) => {
+    loadEnvironment();
+    cleanDb()
+    .then(insertAdminUser)
+    .then((_adminId) => {
+        return done();
+    });
+});
