4a53ca54f898873da18b29468704582d0916474c
Joao611 <joaofvde@gmail.com>
2019-04-26 15:04:34 +0100

    Fix API unit tests in pipeline

--- scripts/write_api_compose.sh
+++ scripts/write_api_compose.sh
@@ -3,9 +3,9 @@ echo "version: '3.7'
 
 services:
   db:
-    image: registry.gitlab.com/feup-tbs/ldso18-19/t2g2/db:$1
+    image: registry.gitlab.com/alexandreaam/lgp-3a/db:$1
   apitest:
-    image: registry.gitlab.com/feup-tbs/ldso18-19/t2g2/apitest:$1
+    image: registry.gitlab.com/alexandreaam/lgp-3a/apitest:$1
     environment:
       - DB_HOST=db
   " >> docker-compose.yml
