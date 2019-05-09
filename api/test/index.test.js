3de515ca08517742e6f1a2854efd22d2cfd70088
Joao611 <joaofvde@gmail.com>
2019-05-10 00:39:28 +0100

    Fix typo

--- api/test/index.test.js
+++ api/test/index.test.js
@@ -59,7 +59,7 @@ async function getTableNames() {
         text: `SELECT table_name
                 FROM information_schema.tables
                 WHERE table_type = 'BASE TABLE'
-                    AND table_schema NOT IN ('pg_catalog', 'information_schema');`
+                    AND table_schema NOT IN ('pg_catalog', 'information_schema')`
     });
 }
 
