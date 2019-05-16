97795315b94ce54324e312758fface30edceef61
Tomás Oliveira <toliveira_97@hotmail.com>
2019-05-16 15:05:19 +0100

    Fix lint

--- api/src/controllers/users.ts
+++ api/src/controllers/users.ts
@@ -184,8 +184,8 @@ export async function getProfilePosts(req, res) {
                 p.visibility, p.date_created, p.date_updated, a.id AS user_id
                     FROM posts p
                         INNER JOIN users a ON (p.author = a.id)
-					WHERE p.author = $1 
-					AND p.talk IS null AND 
+					WHERE p.author = $1
+					AND p.talk IS null AND
 							(p.visibility = 'public'
 							OR (p.visibility= 'private' AND p.author = $2)
 							OR (p.visibility = 'followers'
