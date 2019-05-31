130b669cc2a3f6676e9b6779639ecadb47a588c3
Nelson Almeida <up201505394@fe.up.pt>
2019-05-31 15:27:55 +0100

    fix fetching user interactions (subscriptions and rating) on post

--- web/src/utils/apiUserInteractions.tsx
+++ web/src/utils/apiUserInteractions.tsx
@@ -1,9 +1,9 @@
-import axiosInstance from "./axiosInstance";
+import axiosInstance from './axiosInstance';
 
 export async function apiGetUserInteractions(
   apiGroup: string,
   subjectId: number
 ) {
   const apiUrl = `/${apiGroup}/${subjectId}/user_interactions`;
-  return axiosInstance.post(apiUrl);
+  return axiosInstance.get(apiUrl);
 }
